a59cc08a865ba5475ea16a67b413667c6e413435
Masatoshi SEKI <m_seki@mva.biglobe.ne.jp>
2010-10-21 22:20:26 +0900

    modified install.rb, for 1.9
    import from RWiki/install.rb

--- install.rb
+++ install.rb
@@ -1,28 +1,37 @@
 require 'rbconfig'
 require 'find'
-require 'ftools'
+require 'fileutils'
 
-include Config
+include RbConfig
 
 $srcdir = CONFIG["srcdir"]
-$version = CONFIG["MAJOR"]+"."+CONFIG["MINOR"]
+if CONFIG['ruby_version'] > '1.9'
+  $version = CONFIG["MAJOR"]+"."+CONFIG["MINOR"]+"."+CONFIG["TEENY"]
+else
+  $version = CONFIG["MAJOR"]+"."+CONFIG["MINOR"]
+end
 $libdir = File.join(CONFIG["libdir"], "ruby", $version)
 $archdir = File.join($libdir, CONFIG["arch"])
 $site_libdir = $:.find {|x| x =~ /site_ruby$/}
+$datadir = CONFIG["datadir"]
 if !$site_libdir
   $site_libdir = File.join($libdir, "site_ruby")
 elsif Regexp.new(Regexp.quote($version)) !~ $site_libdir
   $site_libdir = File.join($site_libdir, $version)
 end
 
-def install_rb(srcdir = nil, destdir = $site_libdir)
-  libdir = "lib"
-  libdir = File.join(srcdir, libdir) if srcdir
+def install(basedir, srcdir, destdir)
+  if srcdir
+    target_dir = File.join(srcdir, basedir)
+  else
+    target_dir = basedir
+  end
+  
   path = []
   dir = []
-  Find.find(libdir) do |f|
+  Find.find(target_dir) do |f|
     next unless FileTest.file?(f)
-    next if (f = f[libdir.length+1..-1]) == nil
+    next if (f = f[target_dir.length+1..-1]) == nil
     next if (/CVS$/ =~ File.dirname(f))
     next if (/\.svn/ =~ File.dirname(f))
     path.push f
@@ -32,15 +41,26 @@ def install_rb(srcdir = nil, destdir = $site_libdir)
     next if f == "."
     next if f == "CVS"
     next if f == ".svn"
-    File::makedirs(File.join(destdir, f))
+    FileUtils.mkdir_p(File.join(destdir, f))
   end
   for f in path
     next if (/\~$/ =~ f)
     next if (/^\./ =~ File.basename(f))
-    File::install(File.join("lib", f), File.join(destdir, f), 0644, true)
+    FileUtils.install(File.join(target_dir, f),
+                      File.join(destdir, f),
+                      :mode => 0644,
+                      :verbose => true)
   end
 end
 
+def install_rb(srcdir=nil, destdir=$site_libdir)
+  install("lib", srcdir, destdir)
+end
+
+def install_data(srcdir=nil, datadir=$datadir)
+  install("data", srcdir, datadir)
+end
+    
 def ARGV.switch
   return nil if self.empty?
   arg = self.shift
@@ -61,12 +81,15 @@ def ARGV.req_arg
 end
 
 destdir = $site_libdir
+datadir = $datadir
 
 begin
   while switch = ARGV.switch
     case switch
     when '-d', '--destdir'
       destdir = ARGV.req_arg
+    when '--datadir'
+      datadir = ARGV.req_arg
     else
       raise "unknown switch #{switch.dump}"
     end
@@ -74,9 +97,10 @@ begin
 rescue
   STDERR.puts $!.to_s
   STDERR.puts File.basename($0) + 
-    " -d <destdir>"
+    " [-d <destdir>]" +
+    " [--datadir <datadir>]"
   exit 1
 end    
 
 install_rb(nil, destdir)
-
+install_data(nil, datadir) rescue nil
