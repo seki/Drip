4d644890640d29724c6e01884b72441ad1f36636
Masatoshi SEKI <m_seki@mva.biglobe.ne.jp>
2011-05-18 00:31:23 +0900

    add Dripper

--- lib/my_drip.rb
+++ lib/my_drip.rb
@@ -1,3 +1,29 @@
 require 'drb/drb'
 
 MyDrip = DRbObject.new_with_uri('drbunix:' + File.expand_path('~/.drip/port'))
+
+class Dripper
+  def initialize(drip, bufsiz=10, at_least=10)
+    @drip = drip
+    @cur = nil
+    @bufsiz = bufsiz
+    @at_least = at_least
+  end
+  attr_accessor :cur
+
+  def now
+    @cur ? @drip.key_to_time(@cur) : nil
+  end
+
+  def now=(time)
+    @cur = @drip.time_to_key(time)
+  end
+
+  def past_each(tag=nil)
+    while kv = @drip.older(@cur, tag)
+      @cur, value = kv
+      yield(value)
+    end
+  end
+end
+
