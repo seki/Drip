76f89a8d36ca505da2334d98bbaa0c3f5a1475f7
Masatoshi SEKI <m_seki@mva.biglobe.ne.jp>
2011-06-14 06:15:59 +0900

    fix since_id

--- sample/my_status.rb
+++ sample/my_status.rb
@@ -1,6 +1,15 @@
 # -*- coding: utf-8 -*-
 require 'drip_tw'
 
+def last_tweet_id
+  key = nil
+  while kv = MyDrip.older(key, 'DripDemo MyStatus')
+    key, value = kv
+    return value['id'] if value['id']
+  end
+  nil
+end
+
 def get_user_timeline(app, since_id, max_id)
   count = 3
   begin
@@ -23,10 +32,12 @@ end
 
 app = DripDemo.new
 
+since_id = last_tweet_id
+
 max_id = nil
 timeline = []
 while true
-  max_id, ary = get_user_timeline(app, nil, max_id)
+  max_id, ary = get_user_timeline(app, since_id, max_id)
   break if ary.empty?
   pp [ary[0]['text'], ary[-1]['text']] rescue nil
   sleep 5
